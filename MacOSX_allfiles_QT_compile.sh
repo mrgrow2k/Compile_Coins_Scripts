e269d74083b50b899c982679771b48d499c6cd46
Mr Grow <36313461+mrgrow2k@users.noreply.github.com>
2020-04-29 23:24:12 +0800

    Update MacOSX_allfiles_QT_compile.sh

--- MacOSX_allfiles_QT_compile.sh
+++ MacOSX_allfiles_QT_compile.sh
@@ -8,7 +8,7 @@ cd SDKs
 sudo wget -c https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.11.sdk.tar.xz
 sudo tar -xf MacOSX10.11.sdk.tar.xz
 cd ..
-sudo make -j2 HOST=x86_64-apple-darwin11
+sudo make -j4 HOST=x86_64-apple-darwin14
 cd ..
 sudo ./autogen.sh
 sudo mkdir db4
@@ -20,6 +20,6 @@ sudo make install
 cd ../../
 sudo ./autogen.sh
 sudo ./configure --prefix=`pwd`/depends/x86_64-apple-darwin11
-sudo make -j2
+sudo make -j4
 sudo make deploy
 echo "No strip required for this file! DMG file is located in the same folder as this script."
