1479cfb3d5b9b85f446c84eebe3aae676471ba57
Mr Grow <36313461+mrgrow2k@users.noreply.github.com>
2020-05-01 11:33:59 +0800

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -1,16 +1,18 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install git -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev -y
 sudo apt install -y build-essential libevent-dev libtool autotools-dev automake pkg-config \
-  bsdmainutils curl libboost-all-dev libssl-dev libboost-tools-dev libdb++-dev \
+  bsdmainutils curl libboost-all-dev libssl-dev libboost-tools-dev \
   libevent-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools \
-  libprotobuf-dev protobuf-compiler libevent-dev libboost-all-dev libdb++-dev libssl-dev \
+  libprotobuf-dev protobuf-compiler libevent-dev libboost-all-dev libssl-dev \
   libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev \
   libboost-test-dev libboost-thread-dev g++-arm-linux-gnueabihf gcc-aarch64-linux-gnu nsis libgmp3-dev
 sudo apt install -y g++-mingw-w64-x86-64
 sudo apt install -y software-properties-common
 sudo apt install -y g++-mingw-w64-i686 mingw-w64-i686-dev
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install libminiupnpc-dev -y
 sudo apt-get install libzmq3-dev -y
 sudo apt-get update
