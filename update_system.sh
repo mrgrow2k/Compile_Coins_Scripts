8fc6b3b27e1596e6ccbe4ecaf5c863290c4de263
Mr Grow <36313461+mrgrow2k@users.noreply.github.com>
2020-05-01 21:15:03 +0800

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -13,20 +13,19 @@ sudo apt install -y build-essential libevent-dev libtool autotools-dev automake 
 sudo apt install -y g++-mingw-w64-x86-64
 sudo apt install -y software-properties-common
 sudo apt install -y g++-mingw-w64-i686 mingw-w64-i686-dev
+sudo apt-get install -y gcc g++ git gcc-*-aarch64-linux-gnu g++-*-aarch64-linux-gnu libncurses5-dev
 sudo apt-get install libminiupnpc-dev -y
 sudo apt-get install libzmq3-dev -y
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y curl librsvg2-bin libtiff-tools bsdmainutils cmake imagemagick libcap-dev \
   libz-dev libbz2-dev python-setuptools
-sudo apt-get install -y curl g++-aarch64-linux-gnu \
-  binutils-aarch64-linux-gnu g++-arm-linux-gnueabihf \
+sudo apt-get install -y curl binutils-aarch64-linux-gnu g++-arm-linux-gnueabihf \
   binutils-arm-linux-gnueabihf binutils-gold bsdmainutils \
   g++-aarch64-linux-gnu binutils-aarch64-linux-gnu g++-aarch64-linux-gnu
 sudo apt-get install -y qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev \
     libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev \
     libssl-dev
-sudo apt-get install -y g++-7-aarch64-linux-gnu gcc-7-aarch64-linux-gnu
 sudo apt -y autoremove
 sudo apt-get update
 sudo apt-get -y upgrade
